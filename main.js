a0f1b2c268b689de7679ab7b2aac11428209e6e7
Goliath3 <yourth33@gmail.com>
2024-01-20 18:41:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import './style.css';
-import { modelViewer } from './modelViewer';
+import { modelViewer } from 'https://cdn.jsdelivr.net/gh/Goliath3/panorama@main/modelViewer.js';
 
 modelViewer(document.querySelector('#container'));
